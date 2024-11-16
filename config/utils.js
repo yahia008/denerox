1bc0a5c1bc69aa3ac8ad81b7603904d27f382df8
yahia008 <yahyatijjani99@gmail.com>
2024-11-15 16:16:58 -0800

    work

--- config/utils.js
+++ config/utils.js
@@ -1,5 +1,14 @@
 const jwt = require('jsonwebtoken')
 
 exports.generatejwt = (_id) => {
+
+    if (typeof user !== 'object' || Array.isArray(user)) {  
+        throw new Error("The 'user' parameter must be a plain object.");  
+    }  
+    
+    if (!process.env.ACCESS_TOKEN) {  
+        throw new Error("ACCESS_TOKEN environment variable is not set.");  
+    }  
+
     return jwt.sign({id:_id}, process.env.ACCESS_TOKEN, { expiresIn: "30d" });
 }
